89b926b8e8d3c572fdb85544eb70871a8c5b4557
lirui <lirui2721@126.com>
2018-10-24 16:42:37 +0800

    完成了第8章 日志模块的使用（注意：第7章没有在此源码中）

--- case/first_case.py
+++ case/first_case.py
@@ -3,6 +3,7 @@ import sys
 sys.path.append(r"D:\project_selenium3") #将工程路径加入到Python的搜索路径中
 from business.register_business import RegisterBusiness
 from selenium import webdriver
+from log.user_log import UserLog
 import unittest
 import HTMLTestRunner
 import os
@@ -10,13 +11,19 @@ import time
 
 
 class FirstCase(unittest.TestCase):
-    '''
-    当使用unittest的setup时，就不需要__init__构造函数了，直接使用setup进行初始化
-    '''
+    
+    @classmethod
+    def setUpClass(cls):
+        cls.log = UserLog()
+        cls.logger = cls.log.get_log()
+        cls.file_name = r'D:\selenium_image\test02.png'
+        cls.driver = webdriver.Chrome()
+        cls.driver.get("http://www.5itest.cn/register")
+        cls.driver.maximize_window()  
+
     def setUp(self):
-        self.driver = webdriver.Chrome() #因为driver在其他函数中要使用，所以加上self使其变为全局的
-        self.driver.get("http://www.5itest.cn/register")
-        self.driver.maximize_window()  
+        self.driver.refresh()
+        self.logger.info("this is Chrome")
         self.login = RegisterBusiness(self.driver)
 
     def tearDown(self):
@@ -28,34 +35,37 @@ class FirstCase(unittest.TestCase):
                 dir_path = os.path.dirname(os.getcwd()) #获取上一级目录，即工程目录  
                 file_path = os.path.join(dir_path,'report',screenshot_name) #截图保存路径
                 self.driver.save_screenshot(file_path)
-        self.driver.close()
-        print("后置条件")
+    
+    @classmethod
+    def tearDownClass(cls):
+        cls.log.close_handle()
+        cls.driver.close()
 
 
     def test_login_email_error(self):
-        email_error = self.login.login_email_error('34','user111','111111',r'D:\selenium_image\test02.png')
+        email_error = self.login.login_email_error('272@126.com','user221','111111',self.file_name)
         self.assertFalse(email_error,'测试失败')
     
     def test_login_username_error(self):
-        username_error = self.login.login_name_error('1234@126.com','1112122222222224444','111111',r'D:\selenium_image\test02.png')
+        username_error = self.login.login_name_error('1234@126.com','1112122222222224444','111111',self.file_name)
         self.assertFalse(username_error)
         # if username_error == True:
         #     print("注册成功了，此条case执行失败")
 
     def test_login_password_error(self):
-        password_error = self.login.login_password_error('12345@126.com','user111','11',r'D:\selenium_image\test02.png')
+        password_error = self.login.login_password_error('12345@126.com','user111','11',self.file_name)
         self.assertFalse(password_error)
         # if password_error == True:
         #     print("注册成功了，此条case执行失败")
 
     def test_login_code_error(self):
-        code_error = self.login.login_code_error('34','user111','111111',r'D:\selenium_image\test02.png')
+        code_error = self.login.login_code_error('34','user111','111111',self.file_name)
         self.assertFalse(code_error)
         # if code_error == True:
         #     print("注册成功了，此条case执行失败")
 
     def test_login_success(self):
-        success = self.login.register_succes("111@qq.com","ss222","111111",r"D:\selenium_image\test02.png")
+        success = self.login.register_succes("222@qq.com","ss222","111111",self.file_name)
         self.assertFalse(success)
         # if self.login.register_succes() == True:
         #     print('注册成功')
@@ -66,7 +76,9 @@ class FirstCase(unittest.TestCase):
 if __name__ == '__main__':
     # unittest.main()   
     dir_path = os.path.dirname(os.getcwd()) #获取上一级目录，即工程目录  
+    print(dir_path)
     file_path = os.path.join(dir_path,'report','first_case.html') #获取report文件夹下first_case.html目录路径
+    print(file_path)
     f = open(file_path,'wb')
     suite = unittest.TestSuite()
     suite.addTest(FirstCase('test_login_success'))
---
+++ log/user_log.py
@@ -0,0 +1,50 @@
+#coding=utf-8
+import logging
+import os
+import datetime
+
+class UserLog(object):
+    
+    def __init__(self):
+        self.logger = logging.getLogger(__name__) #创建一个Logger对象
+        # 以下三行为清空上次文件,详细介绍见：https://blog.csdn.net/Chelydra/article/details/79850366
+        #https://www.jb51.net/article/64746.htm
+        # 这为清空当前文件的logging 因为logging会包含所有的文件的logging
+        logging.Logger.manager.loggerDict.pop(__name__)
+        # 将当前文件的handlers 清空
+        self.logger.handlers = []
+        # 然后再次移除当前文件logging配置
+        self.logger.removeHandler(self.logger.handlers)
+        # 这里进行判断，如果logger.handlers列表为空，则添加，否则，直接去写日志
+        if not self.logger.handlers:
+            self.logger.setLevel(logging.DEBUG) #设置等级
+
+            #文件输出名字和路径
+            base_dir = os.path.dirname(os.path.abspath(__file__)) #获取当前文件路径的上一层路径，abspath方法是获得绝对路径
+            log_dir = os.path.join(base_dir,"logs") #获得logs文件夹的路径
+            log_name = datetime.datetime.now().strftime("%Y-%m-%d")+".log" #以当前时间来命名当前log日志的名字
+            log_file_path = os.path.join(log_dir,log_name) #获得整个log日志的完整路径        
+
+            #控制台输出
+            #consle = logging.StreamHandler() #创建处理流
+            #logger.addHandler(consle) #将流加入到Logger中
+            #文件输出日志
+            self.file_handle = logging.FileHandler(log_file_path,'a',encoding='utf-8') 
+            self.file_handle.setLevel(logging.INFO) #设置handle处理的级别
+            formatter = logging.Formatter("%(asctime)s %(filename)s-->%(funcName)s %(lineno)d %(levelname)s---->%(message)s") #格式
+            self.file_handle.setFormatter(formatter)
+            self.logger.addHandler(self.file_handle)
+
+    def get_log(self):
+        return self.logger
+
+    def close_handle(self):      
+        self.logger.removeHandler(self.file_handle) #移除handler
+        self.file_handle.close()
+
+
+if __name__ == '__main__':
+    user = UserLog()
+    log = user.get_log()
+    log.debug("test")
+    user.close_handle()
